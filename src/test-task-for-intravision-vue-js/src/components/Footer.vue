ad9c7497eee29cf39dac8057f923787ff3988ab3
Lev4and <andrey.levchenko.2001@gmail.com>
2024-04-08 17:48:04 +0500

    Implemented setting of Vue.Js project.

---
+++ src/test-task-for-intravision-vue-js/src/components/Footer.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <footer class="py-5 text-center text-body-secondary bg-body-tertiary">
+    <p>© 2024 <a href="https://github.com/Lev4and">Lev4and</a></p>
+    <p class="mb-0">
+      <a href="#">Back to top</a>
+    </p>
+  </footer>
+</template>
---
+++ src/test-task-for-intravision-vue-js/src/components/Header.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="border-bottom lh-1 py-3">
+    <div class="row flex-nowrap justify-content-between align-items-center">
+      <div class="col-4 pt-1"></div>
+      <div class="col-4 text-center">
+        <router-link class="blog-header-logo text-body-emphasis text-decoration-none" to="/">
+          Vending machine
+        </router-link>
+      </div>
+      <div class="col-4 d-flex justify-content-end align-items-center">
+        <router-link class="btn btn-sm btn-outline-secondary" to="/admin"> Admin </router-link>
+      </div>
+    </div>
+  </header>
+</template>
